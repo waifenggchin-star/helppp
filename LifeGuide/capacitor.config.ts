4840043d4e9bb16c5cbf84e5aff4a363ea369e2d
waifenggchin-star <waifenggchin@gmail.com>
2026-02-03 17:06:19 +0800

    rename project to helppp

--- LifeGuide/capacitor.config.ts
+++ LifeGuide/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.lifeguide.app',
-  appName: 'LifeGuide',
+  appName: 'helppp',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
